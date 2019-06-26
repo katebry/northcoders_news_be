cd7c97994f45eac50133a1e4a8f22e0becef22b3
Kate Bryan <katebryan31@gmail.com>
2019-06-26 09:28:31 +0100

    created an error handling function for 405 Method Not Allowed, added to routes

--- routes/articlesRouter.js
+++ routes/articlesRouter.js
@@ -1,15 +1,22 @@
 const articlesRouter = require("express").Router();
+const { sendMethodNotAllowed } = require("../errors/index");
 const {
   sendArticles,
   updateArticle,
-  newComment
+  newComment,
+  sendComments
 } = require("../controllers/articles");
 
 articlesRouter
   .route("/:article_id")
   .get(sendArticles)
-  .patch(updateArticle);
+  .patch(updateArticle)
+  .all(sendMethodNotAllowed);
 
-articlesRouter.route("/:article_id/comments").post(newComment);
+articlesRouter
+  .route("/:article_id/comments")
+  .post(newComment)
+  .get(sendComments)
+  .all(sendMethodNotAllowed);
 
 module.exports = articlesRouter;

681cde2bdc1df1d2b9a26a0d86fcf037bc211ed2
Kate Bryan <katebryan31@gmail.com>
2019-06-27 11:04:57 +0100

    delete method added to commentsRouter

--- routes/commentsRouter.js
+++ routes/commentsRouter.js
@@ -1,10 +1,14 @@
 const commentsRouter = require("express").Router();
 const { sendMethodNotAllowed } = require("../errors/index");
-const { updateCommentVoteCount } = require("../controllers/comments");
+const {
+  updateCommentVoteCount,
+  deleteCommentById
+} = require("../controllers/comments");
 
 commentsRouter
   .route("/:comment_id")
   .patch(updateCommentVoteCount)
+  .delete(deleteCommentById)
   .all(sendMethodNotAllowed);
 
 module.exports = commentsRouter;
